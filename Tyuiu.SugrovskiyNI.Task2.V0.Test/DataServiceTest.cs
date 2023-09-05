b17d66d68c3da431634ea0669ae503eb898b2305
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-09-05 12:21:33 +0500

    Task2

---
+++ Tyuiu.SugrovskiyNI.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SugrovskiyNI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SugrovskiyNI.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            var name = "Никита";
+            var res = DataService.GetMassage(name);
+
+
+
+            Assert.AreEqual("Привет, Никита", res);
+        }
+    }
+}
---
+++ Tyuiu.SugrovskiyNI.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1198fc99-ee96-4a77-8776-4735eae71414")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
